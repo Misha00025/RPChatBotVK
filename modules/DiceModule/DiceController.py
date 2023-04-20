86c1edd3609a71e4a83521fc2de33ce861c6891d
Misha00025 <misha00025@mail.ru>
2023-04-21 02:09:43 +0300

    Добавил смайлы на 1 и 20 при броске к20

--- modules/DiceModule/DiceController.py
+++ modules/DiceModule/DiceController.py
@@ -27,10 +27,11 @@ class DiceController():
         if self.is_correct_parameters(parameters):
             dice = get_first_num(parameters)
             result_dice = self.roll_dice(dice)
-            if (dice == parameters):
+            print(f"dice{dice} = {result_dice}")
+            print(result_dice)
+            if dice == parameters:
                 return f"Результат броска {command}{parameters}: {result_dice}"
             else:
-
                 formula = str(result_dice) + parameters[len(dice):]
                 return f"Результат броска {command}{parameters}: {ne.evaluate(formula)} ({formula})"
         else:
@@ -43,4 +44,8 @@ class DiceController():
 
     def roll_dice(self, dice) -> int:
         result = 1 + int(self._random.random() * int(dice))
+        if result == 20 and int(dice) == 20:
+            result = str(result) + " &#128165;"
+        elif result == 1 and int(dice) == 20:
+            result = str(result) + " &#128128;"
         return result
