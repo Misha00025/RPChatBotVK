ed48f58fdd979aeaa46d77f3d59750a840443653
Misha00025 <102412201+misha00025@users.noreply.github.com>
2024-08-02 12:36:01 +0300

    feat: delete Data Base configs

--- config.py
+++ config.py
@@ -11,15 +11,7 @@ log_file_name = _conf["DEFAULT"]["LogFIle"]
 silence_prefix = _conf["DEFAULT"]["SilencePrefix"]
 st_file_name = _conf_path+_conf["DEFAULT"]["ServiceTokenFile"]
 token_file_name = _conf_path + _conf["VK"]["TokenFile"]
-db_connection_file_name = _conf_path + _conf["DATABASE"]["DbConnectionSettingsFile"]
 
-try:
-    _db_config = configparser.ConfigParser()
-    _db_config.read(db_connection_file_name)
-    connection_settings = _db_config["DATABASE"]
-except:
-    print(f"File \"{db_connection_file_name}\" do not exist!")
-    connection_settings = None
 
 token = open(token_file_name, "r", encoding="utf-8").read()
 service_token = str(open(st_file_name, "r", encoding="utf-8").readline()).strip()
