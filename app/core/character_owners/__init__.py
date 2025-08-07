5662f040b538682a81c6843f49cf4acec2e563b5
Misha00025 <102412201+misha00025@users.noreply.github.com>
2025-08-07 17:57:12 +0300

    feat: save character

--- app/core/character_owners/__init__.py
+++ app/core/character_owners/__init__.py
@@ -1,12 +1,29 @@
 
-characters = {}
+from app.core.DataSaver import load, save
+
+
+_characters = {}
+_save_name = "characters"
 
 
 def set_character(user_id, character_id):
-    characters[str(user_id)] = int(character_id)
+    _characters[str(user_id)] = int(character_id)
 
 def as_character(user_id) -> int:
-    if user_id not in characters:
+    if user_id not in _characters:
         return -1
-    return characters[str(user_id)]
+    return _characters[str(user_id)]
 
+
+def save_characters():
+    save(_characters, _save_name)
+
+
+def load_characters():
+    global _characters
+    err, res = load(_save_name)
+    if not err:
+        _characters = res
+
+
+load_characters()
